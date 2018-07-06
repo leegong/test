ebd6ba2fdc5857edf69b8f35fa2a6cffe9219f05
leegong <lgustb@gmail.com>
2018-07-05 20:17:01 -0400

    Update myScript1.js

--- myScript1.js
+++ myScript1.js
@@ -1,3 +1,3 @@
 function myFunction1() {
-   document.getElementById("demo").innerHTML = "Paragraph changed and use the js file stored in the same fold.";
+   document.getElementById("demo1").innerHTML = "Paragraph changed and use the js file stored in the same fold.";
 }
